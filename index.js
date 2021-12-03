a294a7800594fa3e3d90616e9b71d6a8b782512a
decabooter <decabooter@gmail.com>
2021-12-03 17:13:46 -0600

    starting to get things set up

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require ('express');
 const path = require('path');
+const {engine} = require('express-handlebars');
+
+const app = express();
+
+const PORT = process.env.PORT|| 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
